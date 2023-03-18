a3390a96bacec388949897464ee1ddec84d1a75b
Taylor Donald <alisetaylor3@gmail.com>
2023-03-17 22:12:31 -0500

    Assignment 04 Bootstrap components + SCSS

---
+++ js/script.js
@@ -0,0 +1,15 @@
+(document).ready(function () {
+    // Open modal when the button is clicked
+    ("#myBtn").click(function () {
+        ("#myModal").modal();
+    });
+});
+
+
+(document).ready(function () {
+    // Open modal when the button is clicked
+    ("#loginBtn").click(function () {
+        ("#loginModal").modal();
+    });
+});
+
